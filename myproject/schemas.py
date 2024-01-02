a03bfbfa5636fd525137f9d1c1b80d7b3a883076
Jorben Sterckx <113974674+JorbenSterckx@users.noreply.github.com>
2024-01-02 16:53:57 +0100

    Update schemas.py

--- myproject/schemas.py
+++ myproject/schemas.py
@@ -39,3 +39,19 @@ class Movie(MovieBase):
 
     class Config:
         orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+
+    class Config:
+        orm_mode = True
